d60fc7f9b4074a27826af073afa8878a2ff78c17
Eduardo San Martin Morote <posva13@gmail.com>
2024-05-10 21:58:58 +0200

    fix: remove global build from package
    Fix #1

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,4 +1,7 @@
+import fs from 'node:fs'
 import { type Options, defineConfig } from 'tsup'
+
+const pkg = JSON.parse(fs.readFileSync('./package.json', 'utf-8'))
 
 const commonOptions = {
   splitting: false,
@@ -6,6 +9,15 @@ const commonOptions = {
   format: ['cjs', 'esm'],
   dts: true,
   target: 'esnext',
+  banner: {
+    js: `
+/*!
+ * ${pkg.name} v${pkg.version}
+ * (c) 2024-present Eduardo San Martin Morote
+ * @license ${pkg.license}
+ */
+`.trim(),
+  },
 } satisfies Options
 
 export default defineConfig([
